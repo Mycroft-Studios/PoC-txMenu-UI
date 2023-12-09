7f839531b03b54262a9237a529ae4c7f1e66626d
Kasey FItton <kaseyfitton@hotmail.co.uk>
2023-12-09 23:48:11 +0000

    tweak(PlayerCard): remove context menu

--- app/pages/PlayerCard.tsx
+++ app/pages/PlayerCard.tsx
@@ -1,5 +1,3 @@
-import {ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger} from "@/components/ui/context-menu";
-import {Button} from "@/components/ui/button";
 import {PersonStandingIcon} from "lucide-react";
 import {PlayerDetails} from "@/app/pages/PlayerList";
 import {Progress} from "@/components/ui/progress";
@@ -18,8 +16,7 @@ function getIndicatorColor(health: number) {
 export function PlayerCard(props: {Player: PlayerDetails}) {
 
     return (
-        <ContextMenu>
-            <ContextMenuTrigger className="grid grid-cols-3 gap-4" style={{marginLeft: "10px"}}>
+            <div className="grid grid-cols-3 gap-4" style={{marginLeft: "10px"}}>
                     <div style={{fontSize: "18px"}}>
                         <div className="flex flex-row gap-2" style={{justifyContent: "left"}}>
                             <p><PersonStandingIcon /></p>
@@ -28,12 +25,6 @@ export function PlayerCard(props: {Player: PlayerDetails}) {
                         </div>
                         <Progress value={props.Player.health} className="h-2" style={{marginTop: "5px", width: "300px"}} indicatorColor={getIndicatorColor(props.Player.health)}/>
                     </div>
-            </ContextMenuTrigger>
-            <ContextMenuContent className="bg-card" style={{width: "100px", height: "100px"}}>
-                <ContextMenuItem>Warn</ContextMenuItem>
-                <ContextMenuItem>Kick</ContextMenuItem>
-                <ContextMenuItem>Ban</ContextMenuItem>
-            </ContextMenuContent>
-        </ContextMenu>
+            </div>
     );
 }
