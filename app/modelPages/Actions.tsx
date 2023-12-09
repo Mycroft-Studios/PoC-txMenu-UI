4495fc89776c08e51f81ecfe6febfaea51248262
Kasey FItton <kaseyfitton@hotmail.co.uk>
2023-12-09 22:12:04 +0000

    refactor(modalPages/Actions): Make Options Dynamic

--- app/modelPages/Actions.tsx
+++ app/modelPages/Actions.tsx
@@ -2,56 +2,39 @@ import React from "react";
 import {Button} from "@/components/ui/button";
 import {ShieldAlertIcon, ShieldCheckIcon, ShieldXIcon} from "lucide-react";
 
+const Options = [
+    {
+        "name": "Moderation",
+        "actions": ["DM", "Warn", "Kick", "Give Admin"]
+    },
+    {
+        "name": "Interaction",
+        "actions": ["Heal", "Goto", "Bring", "Spectate", "Toggle Freeze"]
+    },
+    {
+        "name": "Trolling",
+        "actions": ["Make Drunk", "On Fire", "Wild Attack"]
+    }
+]
+
 export function Actions() {
     return (
         <div>
             <p className="text-xl">Player Actions</p>
-            <div className="grid grid-rows-6 gap-2" style={{marginBottom: "8px"}}>
-                 <p style={{marginTop: "15px", fontSize: "17px"}}>Moderation</p>
-                 <div className="flex flex-row h-5 space-x-3">
-                        <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                            DM
-                        </Button>
-                        <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                            Warn
-                        </Button>
-                        <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                           Kick
-                        </Button>
-                         <Button variant="outline" className="w-25" style={{justifyContent: "center"}}>
-                             Give Admin
-                         </Button>
-                 </div>
-                <p style={{marginTop: "15px", fontSize: "17px"}}>Interaction</p>
-                <div className="flex flex-row h-5 space-x-3">
-                    <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                        Heal
-                    </Button>
-                    <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                        Goto
-                    </Button>
-                    <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                        Bring
-                    </Button>
-                    <Button variant="outline" className="w-25" style={{justifyContent: "center"}}>
-                        Spectate
-                    </Button>
-                    <Button variant="outline" className="w-25" style={{justifyContent: "center"}}>
-                        Toggle Freeze
-                    </Button>
-                </div>
-                <p style={{marginTop: "15px", fontSize: "17px"}}>Trolling</p>
-                <div className="flex flex-row h-6 space-x-3">
-                    <Button variant="outline" className="w-25" style={{justifyContent: "center"}}>
-                        Make Drunk
-                    </Button>
-                    <Button variant="outline" className="w-20" style={{justifyContent: "center"}}>
-                        On Fire
-                    </Button>
-                    <Button variant="outline" className="w-25" style={{justifyContent: "center"}}>
-                        Wild Attack
-                    </Button>
-                </div>
+            <div className="flex flex-col" style={{marginBottom: "8px"}}>
+                {Options.map((option) => (
+                    <div key={option.name}>
+                        <p style={{marginTop: "15px", fontSize: "17px", marginBottom: "5px"}}>{option.name}</p>
+                        <div className="flex flex-row h-10 space-x-3">
+                            {option.actions.map((action) => (
+                                <Button key={action} variant="outline" className="" style={{justifyContent: "center"}}>
+                                    {action}
+                                </Button>
+                            ))}
+                        </div>
+                    </div>
+                    ))
+                }
             </div>
         </div>
     );
