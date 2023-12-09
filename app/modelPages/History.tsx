e2a3f83cb24ce0d37685c7e6dcb0a41d7491d025
Kasey FItton <kaseyfitton@hotmail.co.uk>
2023-12-09 22:47:22 +0000

    refactor(modalPages/History): Dynamic Listing

--- app/modelPages/History.tsx
+++ app/modelPages/History.tsx
@@ -5,17 +5,30 @@ import {ScrollArea} from "@/components/ui/scroll-area";
 import React from "react";
 import {PlayerModel} from "@/app/pages/PlayerModel";
 import {PlayerDetails} from "@/app/pages/PlayerList";
-import {HistoryCard} from "@/app/modelPages/HistoryCard";
+import {HistoryCard, HistoryCardProps} from "@/app/modelPages/HistoryCard";
 
-const cards = ["", "", ""]
-export function History() {
+const cards: HistoryCardProps[] = [
+    {
+        reason: "Cheating",
+        issuer: " Mycroft",
+        date: "2021-09-01 12:00:00",
+        type: "BANNED"
+    },
+    {
+        reason: "Cheating",
+        issuer: " Mycroft",
+        date: "2021-09-01 12:00:00",
+        type: "WARNED"
+    },
+];
+export function History(props: HistoryCardProps) {
     return (
         <div>
             <p className="text-xl">Related History</p>
             <div className="flex flex-row gap-0">
                 <ScrollArea className="h-[292px] w-[550px]">
-                    {cards.map((s: string) => (
-                            <HistoryCard key="s" />
+                    {cards.map((s: HistoryCardProps) => (
+                            <HistoryCard key={s.reason} {... s}/>
                     ))}
                 </ScrollArea>
             </div>
--- app/modelPages/HistoryCard.tsx
+++ app/modelPages/HistoryCard.tsx
@@ -3,23 +3,33 @@ import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label"
 import React from "react";
 
-export function HistoryCard() {
+export interface HistoryCardProps {
+    reason: string;
+    issuer: string;
+    date: string;
+    type: string;
+}
+
+export function HistoryCard(props: HistoryCardProps) {
+    const {reason, issuer, date, type} = props;
+    const color = type == "BANNED" ? "var(--accent)" : "var(--warning)";
+
     return (
-        <div className={"flex flex-col space-x-2 bg-secondary mt-1 pr-2 border-l-4 border-l-destructive"} style={{marginTop: "5px", fontSize: "15px"}}>
+        <div className={"flex flex-col space-x-2 bg-secondary mt-1 pr-2 border-l-4"} style={{marginTop: "5px", fontSize: "15px", borderLeftColor: color}}>
             <div className="flex flex-row">
-                <div>
-                    <strong className="ml-3">WARNED by </strong>
-                    <strong>Mycroft</strong>
+                <div className="flex flex-row ml-3 flex-wrap w-52 gap-1">
+                    <strong>{type} by </strong>
+                    <strong>{issuer}</strong>
                 </div>
-                <div className="ml-40 flex flex-row">
-                    <p className="ml-4 text-muted-foreground font-mono font-light" style={{fontSize: "14px"}}>04/12/2023 12:00</p>
+                <div className="ml-20 flex flex-row">
+                    <p className="ml-4 text-muted-foreground font-mono font-light" style={{fontSize: "14px"}}>{date}</p>
                     <Button variant="outline" className="w-16 ml-2 rounded-sm" style={{justifyContent: "center", height: "20px", marginTop: "1px"}}>
                         <p style={{fontSize: "13px"}}>Revoke</p>
                     </Button>
                 </div>
             </div>
             <div>
-                <p className="ml-2">This is the reason.</p>
+                <p className="ml-2">{reason}</p>
             </div>
         </div>
     );
